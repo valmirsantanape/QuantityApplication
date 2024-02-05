286fcb934823d539e3b2975db1ac0d9e57f4171c
bowie <valmirsantanape@gmail.com>
2024-02-05 19:15:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# QuantityApplication
+ App simples que encrementa valor 1 e decrementa valor 1 ao clicar no botão de mais e menos respectivamente
---
+++ src/program.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class program {
+    public static void main(String[] args) {
+
+       JFrame frame = new JFrame("Quantity Application");
+
+       JLabel label = new JLabel("Quantity");
+       JTextField textFilde = new JTextField();
+       JButton buttonMais = new JButton("+");
+       JButton buttonMenos = new JButton("-");
+       frame.setLayout(new GridLayout(1,4));
+
+       frame.add(label);
+       frame.add(textFilde);
+       frame.add(buttonMais);
+       frame.add(buttonMenos);
+
+       frame.setSize(300, 100);
+       frame.setVisible(true);
+
+       int cont = 0;
+       textFilde.setText(Integer.toString(cont));
+
+       buttonMais.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+
+               int cont = Integer.parseInt(textFilde.getText()) + 1;
+
+               textFilde.setText(Integer.toString(cont));
+
+           }
+       });
+
+       buttonMenos.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               int cont = Integer.parseInt(textFilde.getText()) - 1;
+               textFilde.setText(Integer.toString(cont));
+
+           }
+       });
+
+    }
+
+
+}
